75223be6a7ce2531ef60f031854de73bf62d7f81
Zi-long QIU <zilong.qiu@gmail.com>
2017-01-08 02:35:27 +0100

    [master] feat: update project files

--- aliases/subtree.zsh
+++ aliases/subtree.zsh
@@ -1,5 +1,19 @@
 # Aliases
 
-RUN_SCRIPTS='sh ./scripts/subtree'
+GIT=$(which git)
 
-alias spull=$RUN_SCRIPTS/pull.sh
+# Check if git exist
+if [ ! -x "$GIT" ]
+then
+    echo "[WARNING] 'git' is not installed on the system"
+    return
+fi
+
+SUBTREE_LIB=$DIR_LIB/subtree
+
+alias slist=$SUBTREE_LIB/list.zsh
+#alias spull=$SUBTREE_LIB/pull.sh
+#alias spush=$SUBTREE_LIB/push.sh
+#alias sco=$SUBTREE_LIB/checkout.sh
+#alias scob=$SUBTREE_LIB/checkoutb.sh
+#alias stag=$SUBTREE_LIB/tag.sh
---
+++ lib/subtree/list.zsh
@@ -0,0 +1,3 @@
+# List all git remotes
+
+git remote -v
