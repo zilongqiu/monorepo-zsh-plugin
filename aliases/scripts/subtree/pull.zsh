b5d5ecbeb975e05e8897f7d5b6fdbe3b254f0b18
Zi-long QIU <zilong.qiu@gmail.com>
2017-01-08 11:08:53 +0100

    [master] feat: add pull script

---
+++ aliases/scripts/subtree/pull.zsh
@@ -0,0 +1,11 @@
+
+git remote -v | while read line
+do
+    echo $line
+    if [[ $line == *"(fetch)" ]]
+    then
+        echo "*** Pulling $line"
+	git subtree pull --prefix=Apps/test --squash -m 'Merge $line into project' app-test git_current_branch
+        echo "\n"
+    fi
+done
