8a5acb198730951fbea183ab75f7b7fb3d91add9
Zi-long QIU <zilong.qiu@gmail.com>
2017-01-08 02:48:42 +0100

    [master] feat: fix subtree slist execution

--- aliases/subtree.zsh
+++ aliases/subtree.zsh
@@ -5,14 +5,14 @@ GIT=$(which git)
 # Check if git exist
 if [ ! -x "$GIT" ]
 then
-    echo "[WARNING] 'git' is not installed on the system"
+    echo "[WARNING] 'git' is not installed on the system!"
     return
 fi
 
-SCRIPTS=$DIR_ALIAS/scripts/subtree
+SCRIPTS="sh $DIR_ALIAS/scripts/subtree"
 
-alias slist=$SCRIPTS/list.zsh
-#alias spull=$SCRIPTS/pull.sh
+alias slist=$SCRIPTS/list.zsh"
+#alias spull="$SCRIPTS/pull.sh
 #alias spush=$SCRIPTS/push.sh
 #alias sco=$SCRIPTS/checkout.sh
 #alias scob=$SCRIPTS/checkoutb.sh
