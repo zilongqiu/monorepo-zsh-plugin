b99b840d667c8cd5d9216768529f4d4bd875e38a
Zi-long QIU <zilong.qiu@gmail.com>
2017-01-08 13:16:20 +0100

    [master] feat: update spull command with automatic sequences

--- aliases/scripts/subtree/pull.zsh
+++ aliases/scripts/subtree/pull.zsh
@@ -1,11 +1,18 @@
 
 PULL_IDENTIFIER="(fetch)"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 git remote -v | while read line
 do
     if [[ $line == *"${PULL_IDENTIFIER}" && $line != "origin"* ]]
     then
-        echo "*** Pulling ${line%$PULL_IDENTIFIER}"
-	git subtree pull --prefix=Apps/test --squash -m 'Merge $line into project' app-test ${git_current_branch}
+	PROJECT_DATA=($line)
+        PROJECT_NAME=${PROJECT_DATA[0]}
+        PROJECT_URL=${PROJECT_DATA[1]}
+        PROJECT_DIR="$(tr '[:lower:]' '[:upper:]' <<< ${PROJECT_NAME:0:1})${PROJECT_NAME:1}"
+        PREFIX_DIR=${PROJECT_DIR%-*}s/${PROJECT_DIR##*-}
+        echo "*** Pulling $PROJECT_NAME ($PROJECT_URL)"
+	git subtree pull --prefix=$PREFIX_DIR --squash -m "Merge $PROJECT_NAME into project" $PROJECT_NAME $CURRENT_BRANCH
+        echo ""
     fi
 done
