76db4b183d0ec490c2310678e2c72a250e613253
Zi-long QIU <zilong.qiu@gmail.com>
2017-01-08 11:15:21 +0100

    [master] feat: update git_current_branch call in spull command

--- aliases/scripts/subtree/pull.zsh
+++ aliases/scripts/subtree/pull.zsh
@@ -5,7 +5,7 @@ do
     if [[ $line == *"(fetch)" ]]
     then
         echo "*** Pulling $line"
-	git subtree pull --prefix=Apps/test --squash -m 'Merge $line into project' app-test git_current_branch
+	git subtree pull --prefix=Apps/test --squash -m 'Merge $line into project' app-test $(git_current_branch)
         echo "\n"
     fi
 done
